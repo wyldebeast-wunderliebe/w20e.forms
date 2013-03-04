6c2ad042ba37e96642a7fedf99f31727089ad13c
Huub Bouma <bouma@w20e.com>
2013-03-04 12:35:36 +0100

    splitted cardgroup rendering to bootstrap and default templates

--- w20e/forms/rendering/html/cardgroup.py
+++ w20e/forms/rendering/html/cardgroup.py
@@ -13,22 +13,16 @@ class CardGroupRenderer:
 
         fmtmap = renderer.createFormatMap(form, renderable, **kwargs)
 
-        str_out = StringIO()
-        sub_out = codecs.getwriter('utf-8')(str_out)
-
-        print >> sub_out, """<div class="tabs">"""
-
-        for sub_renderable in renderable.getRenderables():
-            print >> sub_out, """<div class="tab" id="tab-%s">%s</div>""" % (sub_renderable.id, sub_renderable.label)
-
-        print >> sub_out, "</div>"
-
-        for sub_renderable in renderable.getRenderables():
-            sub_renderable.extra_classes = (sub_renderable.extra_classes or "") + " card"
-            renderer.render(form, sub_renderable, sub_out, **kwargs)
+        def render_subs(sub):
+            """ render the sub renderable """
+            str_out = StringIO()
+            out = codecs.getwriter('utf-8')(str_out)
+            sub.extra_classes = sub.extra_classes or ""
+            renderer.render(form, sub, out, **kwargs)
+            return out.getvalue().decode('utf-8')
 
         print >> out, get_template('cardgroup')(
             group=renderable,
-            content=str_out.getvalue().decode("utf-8"),
-            fmtmap=fmtmap
+            fmtmap=fmtmap,
+            render_subs = render_subs
             )
