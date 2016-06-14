6633f6c92cb822f2c0db1db5f5fae9dbff582707
Huub Bouma <bouma@w20e.com>
2016-06-14 13:37:09 +0200

    small hack to fix multiselect select rendering

--- w20e/forms/rendering/html/select.py
+++ w20e/forms/rendering/html/select.py
@@ -2,6 +2,7 @@ from templates import get_template
 from w20e.forms.rendering.interfaces import IControlRenderer
 from zope.interface import implements
 from w20e.forms.registry import Registry
+from types import ListType
 
 
 class SelectRenderer(object):
@@ -29,6 +30,14 @@ class SelectRenderer(object):
         opts.extend(renderable.options)
 
         value = form.data[renderable.bind]
+
+        # some hacking for multiple type. sometimes the saved data is just a
+        # plain old type (int) and we expect a list of strings in the template
+        if fmtmap['multiple'] and fmtmap['multiple'].lower() == 'true':
+            value = form.getFieldValue(renderable.id, lexical=False)
+            if type(value) == ListType:  # just to be sure?
+                value = [str(val) for val in value]
+
         if renderable.format == "full":
 
             print >> out, get_template('select_full')(
