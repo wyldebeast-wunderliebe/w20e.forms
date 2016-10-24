c39dc543c354136e93b022b32368fe82d620bf7f
Huub Bouma <bouma@w20e.com>
2016-10-24 22:05:19 +0200

    back to pyduktape since it installs on osx as well

--- w20e/forms/evaluator.py
+++ w20e/forms/evaluator.py
@@ -1,74 +1,14 @@
 """
 Evaluate the given expression
 Either use the default python eval() method to evaluate the expression
-or use javascript (spidermonkey)
+or use javascript (pyduktape)
 """
-try:
-    import spidermonkey
-except ImportError:
-    spidermonkey = None
 
-# from zope import interface
-# from zope.component import getUtility
+import pyduktape
 import threading
 
 
-# Based on tornado.ioloop.IOLoop.instance() approach.
-# See https://github.com/facebook/tornado
-class SingletonMixin(object):
-    __singleton_lock = threading.Lock()
-    __singleton_instance = None
-
-    @classmethod
-    def instance(cls):
-        if not cls.__singleton_instance:
-            with cls.__singleton_lock:
-                if not cls.__singleton_instance:
-                    cls.__singleton_instance = cls()
-        return cls.__singleton_instance
-
-
-class EvalJSUtil(SingletonMixin):
-
-    """ There seems to be a memory leak issue with spidermonkey
-    context creation. So wrapping this in a util class and
-    ceating a single context which is reused solves the issue I hope
-    """
-
-    def __init__(self):
-        # note: need the runtime here, to prevent
-        # "Failed to allocate new JSRuntime" error
-
-        if spidermonkey:
-            self.spidermonkey_rt = spidermonkey.Runtime()
-            self.spidermonkey_cx = self.spidermonkey_rt.new_context()
-
-    def eval(self, expression, _globals, _locals=None):
-        """ import spidermonkey and eval the expression  """
-
-        if not spidermonkey:
-            raise ("Spidermonkey not available. "
-                   "Please install python-spidermonkey")
-
-        for k, v in _globals.items():
-            self.spidermonkey_cx.add_global(k, v)
-
-        if _locals:
-            for k, v in _locals.items():
-                self.spidermonkey_cx.add_global(k, v)
-
-        result = self.spidermonkey_cx.execute(expression)
-
-        # clean up globals, so we can reuse the context
-        # and work around the memory leak
-        for k, v in _globals.items():
-            self.spidermonkey_cx.rem_global(k)
-
-        if _locals:
-            for k, v in _locals.items():
-                self.spidermonkey_cx.rem_global(k)
-
-        return result
+threadLocal = threading.local()
 
 
 def eval_python(expression, _globals, _locals=None):
@@ -76,10 +16,36 @@ def eval_python(expression, _globals, _locals=None):
     return eval(expression, _globals, _locals)
 
 
-evalJSUtil = EvalJSUtil()
-
-
 def eval_javascript(expression, _globals, _locals=None):
-    """ import spidermonkey and eval the expression  """
+    """ try to import pyduktape and eval the expression  """
 
-    return evalJSUtil.eval(expression, _globals, _locals)
+    result = None
+
+    # there's a memory leak when a duktapecontext is removed from the scope.
+    # the garbagecollection doesn't work as expected.
+    # workaround is to have one duktapecontext per thread
+    # (we can't have 1 global, since pyduktape will raise a
+    # DuktapeThreadError, so we get around this using a threadlocal)
+    context = getattr(threadLocal, 'context', None)
+    if context is None:
+        context = pyduktape.DuktapeContext()
+        threadLocal.context = context
+
+    context.set_globals(**_globals)
+
+    # pyduktape doesn's have locals so insert it into the globals instead
+    if _locals:
+        context.set_globals(**_locals)
+
+    result = context.eval_js(expression)
+
+    # clean up globals (since it's being reused in threadlocal)
+    # there is no way to unset a global variable so just set all to null
+    for k, v in _globals.items():
+        context.set_globals(k=None)
+
+    if _locals:
+        for k, v in _locals.items():
+            context.set_globals(k=None)
+
+    return result
