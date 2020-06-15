f8a35a9fd8c9f5a18f4f57fde133f434de133d15
Huub Bouma <huub@pythonunited.com>
2020-06-15 17:38:52 +0200

    add some frequently used shortcuts to the JS evaluator to increase speed

--- w20e/forms/evaluator.py
+++ w20e/forms/evaluator.py
@@ -7,10 +7,11 @@ or use javascript (pyduktape)
 import pyduktape
 import threading
 from logging import getLogger
+import re
 
 
 threadLocal = threading.local()
-LOGGER = getLogger('w20e.form')
+LOGGER = getLogger("w20e.form")
 
 
 def eval_python(expression, _globals, _locals=None):
@@ -23,6 +24,50 @@ def eval_javascript(expression, _globals, _locals=None):
 
     result = None
 
+    # first some fast shortcuts which don't require a JS parser
+    shortcuts = {
+        "1": 1,
+        "true": True,
+        "0": 0,
+        "false": False,
+    }
+    if expression in shortcuts:
+        return shortcuts[expression]
+
+    # now for some other frequent occuring expressions
+    # like: data['somefield']==1
+    match = re.match(r'^ *data\[[\'"](\w+)[\'"]\] *== *(\d+) *$', expression)
+    if match:
+        left = _globals["data"][match.group(1)]
+        right = match.group(2)
+        return left == right or left == str(right)
+
+    # like: data['somefield']===1
+    match = re.match(r'^ *data\[[\'"](\w+)[\'"]\] *=== *(\d+) *$', expression)
+    if match:
+        left = _globals["data"][match.group(1)]
+        right = match.group(2)
+        return left == right
+
+    # like: data['somefield']=='value'
+    match = re.match(r'^ *data\[[\'"](\w+)[\'"]\] *== *[\'"](\w+)[\'"] *$', expression)
+    if match:
+        left = _globals["data"][match.group(1)]
+        right = match.group(2)
+        return left == right or left == str(right)
+
+    # like: data['somefield']==='value'
+    match = re.match(r'^ *data\[[\'"](\w+)[\'"]\] *== *[\'"](\w+)[\'"] *$', expression)
+    if match:
+        left = _globals["data"][match.group(1)]
+        right = match.group(2)
+        return left == str(right)
+
+    # expressions like: data['somefield']
+    match = re.match(r'^ *data\[[\'"](\w+)[\'"]\] *$', expression)
+    if match:
+        return _globals["data"][match.group(1)]
+
     # there's a memory leak when a duktapecontext is removed from the scope.
     # the garbagecollection doesn't work as expected.
     # workaround is to have one duktapecontext per thread
@@ -31,7 +76,7 @@ def eval_javascript(expression, _globals, _locals=None):
     # update: there seems to be a memory leak fix out but it seems that
     # keeping the context alive is much faster.
     # js2py is another library which works, but it's much slower
-    context = getattr(threadLocal, 'context', None)
+    context = getattr(threadLocal, "context", None)
     if context is None:
         context = pyduktape.DuktapeContext()
         threadLocal.context = context
@@ -40,12 +85,12 @@ def eval_javascript(expression, _globals, _locals=None):
     # for those cases just convert them to a string and hope for the best..
     safe_data = {}
     JS_MAX_NUM = 9007199254740991  # math.pow(2, 53) -1
-    for (k, v) in _globals['data'].as_dict().items():
+    for (k, v) in _globals["data"].as_dict().items():
         if isinstance(v, int) and not -JS_MAX_NUM < v < JS_MAX_NUM:
             safe_data[k] = str(v)
         else:
             safe_data[k] = v
-    _globals['data'] = safe_data
+    _globals["data"] = safe_data
 
     context.set_globals(**_globals)
 
@@ -66,7 +111,7 @@ def eval_javascript(expression, _globals, _locals=None):
     try:
         result = context.eval_js(expression)
     except pyduktape.JSError as err:
-        LOGGER.warning('error evaluating js expression: {}'.format(expression))
+        LOGGER.warning("error evaluating js expression: {}".format(expression))
         LOGGER.warning(err)
         result = None
 
