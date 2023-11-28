0973fccd90e41f6a3bd00511d9127c1a29b49541
Huub Bouma <huub@pythonunited.com>
2023-11-28 10:17:05 +0100

    change masked-eval.js

--- w20e/forms/masked-eval.js
+++ w20e/forms/masked-eval.js
@@ -2,19 +2,20 @@
 
 module.exports = (function () {
   function maskedEval(expression, context) {
-    const maskedContext = Object.create(null);
 
-    for (let prop in globalThis) {
-      maskedContext[prop] = undefined;
+    try {
+        const contextKeys = Object.keys(context);
+        const contextValues = contextKeys.map(key => context[key]);
+
+        // Create a function with the provided expression and context variables
+        const evalFunction = new Function(...contextKeys, `return ${expression}`);
+        const result = evalFunction(...contextValues);
+        return result;
+    } catch (error) {
+        // Handle any errors that may occur during evaluation
+        console.error('Error evaluating expression:', error.message);
+        return null;
     }
-
-    for (let prop in context) {
-      if (context.hasOwnProperty(prop)) {
-        maskedContext[prop] = context[prop];
-      }
-    }
-
-    return new Function("context", `with(context) { return ${expression} }`)(maskedContext);
   }
 
   return {
